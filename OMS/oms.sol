2700ba5e6eeb5bc6897f8c075845f7d0d2b79fc5
echelr73 <eche_lr73@hotmail.com>
2023-01-05 09:13:46 -0300

    Variables Iniciales

--- OMS/oms.sol
+++ OMS/oms.sol
@@ -1,5 +1,17 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.4 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-contract oms{}
+contract OMS_COVID{
+
+    //Direccion de la OMS => Owner / Dueño del contrato
+    address public OMS;
+
+    //Constructor del contrato
+    constructor() public{
+        OMS = msg.sender;
+    }
+
+    //Mapping para relacionar los centros de salud (direccion/address) con la validez del sistema de gestion
+    mapping (address => bool) Validacion_CentroSalud;
+}
